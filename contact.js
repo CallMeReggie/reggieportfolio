7434902c6e2bae9284cca0fee833f876cc0e6869
CallMeReggie <reggienavarro0728@gmail.com>
2026-03-30 19:24:52 +0800

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,18 @@
+/* js/contact.js */
+window.portfolioData = window.portfolioData || {};
+
+/*
+  EDIT THIS FILE WHEN YOU WANT TO CHANGE:
+  - email
+  - phone number
+  - LinkedIn
+  - location
+*/
+
+window.portfolioData.contact = {
+  title: "Contact",
+  subtitle: "Professional contact details for recruiters and hiring teams.",
+  email: "reggienavarro0728@gmail.com",
+  phone: "+63 963 975 4604",
+  location: "San Marcus, Jerome, Agdao, Davao City",
+};
---
+++ education.js
@@ -0,0 +1,19 @@
+/* js/education.js */
+window.portfolioData = window.portfolioData || {};
+
+/*
+  EDIT THIS FILE WHEN YOU WANT TO CHANGE:
+  - school details
+  - certifications
+  - training
+*/
+
+window.portfolioData.education = {
+  title: "Education",
+  subtitle: "My current academic background",
+  details: [
+    { label: "School", value: "University of Mindanao" },
+    { label: "Program", value: "Bachelor of Science in Computer Engineering" },
+    { label: "Current Year Level", value: "2nd Year" }
+  ]
+};
